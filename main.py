2675da1909f8270e40382e77675ecfeec5eaba8b
seanald <sean_m_fitzgerald@tutanota.com>
2020-05-03 13:09:41 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import (
+        NumericProperty, ReferenceListProperty, ObjectProperty)
+from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
+
+class CardApp(App):
+    def build(self):
+        game = CardGame()
+        game.run()
---
+++ pip-installs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+pip install --upgrade pip
+pip install --upgrade wheel
+pip install --upgrade requests
+pip install --upgrade kivy
+pip install --upgrade cython
+pip install --upgrade https://github.com/kivy/buildozer/archive/master.zip
